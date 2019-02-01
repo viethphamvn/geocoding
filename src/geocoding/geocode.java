206e526b15426ba288a48ae759c122a8eed3f110
Viet Pham <tug36669@temple.edu>
2019-01-31 22:21:56 -0500

    Test Version Done

--- src/geocoding/geocode.java
+++ src/geocoding/geocode.java
@@ -5,16 +5,16 @@
  */
 package geocoding;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.*;
+
 
 
 /**
@@ -27,41 +27,31 @@ public class geocode {
     String API_key = "AIzaSyC4i8VAgGQWY2jgzSQSsoXgshRX0Mtnzz8"; 
     
     public geocode(String address) throws IOException{
- 
+        String json = "";
+        
         try {
             HttpURLConnection connection = null;
             URL url = new URL(endpoint + "address=" +address + "&key=" + API_key);
 
             connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-        //Send request
-            DataOutputStream wr = new DataOutputStream (
-            connection.getOutputStream());
-            wr.close();
-
-        //Get Response  
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-            String line;
-            while ((line = rd.readLine()) != null) {
-              response.append(line);
-              response.append("\n");
+            
+            connection.setRequestMethod("GET");
+            connection.connect();
+            
+            int responsecode = connection.getResponseCode();
+            
+            Scanner sc = new Scanner(url.openStream());
+            while (sc.hasNext()){
+                json += sc.nextLine();
             }
-            rd.close();
-            int index = response.indexOf("\"location");
-            while(true){
-                System.out.print(response.charAt(index++));
-                if (response.charAt(index) == '}'){
-                    System.out.println(response.charAt(index));
-                    break;
-                }
-            }
+            sc.close();
+            
+            JSONObject obj = new JSONObject(json);
+            JSONObject res = obj.getJSONArray("results").getJSONObject(0);
+            JSONObject loc = res.getJSONObject("geometry").getJSONObject("location");
+            
+            System.out.println("lat: " + loc.getDouble("lat") + ", lng: " + loc.getDouble("lng"));
+            System.out.println("Head \\u003cb\\u003ewest\\u003c/b\\u003e toward \\u003cb\\u003eN Carlisle St\\u003c/b\\u003e");
         } catch (MalformedURLException ex) {
             Logger.getLogger(geocode.class.getName()).log(Level.SEVERE, null, ex);
         }
